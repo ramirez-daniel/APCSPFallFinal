dfdef34a9dc76d7ce1755c062804ee97f105463b
Daniel A Ramirez <dr82913@srcs.loc>
2017-12-15 10:42:10 -0800

    Finished timesTable function

--- final_challenge_functions.js
+++ final_challenge_functions.js
@@ -2,6 +2,14 @@ function checkPrime(number); {
 
 }
 
-function timesTable(n); {
-
+function timesTable(number); {
+    document.write("<table>");
+    for(i = 1; i <= number; i++){
+        document.write("<tr>");
+        for(j = 1; j <= number; j++){
+            document.write("<td>"; i * j;"</td>");
+        }
+        document.write("</tr>");
+    }
+    document.write("</table>");
 }
