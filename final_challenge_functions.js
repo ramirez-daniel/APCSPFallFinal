b4f8df2732b69f378aac6f0b00ac30d0d76f17d9
ramirez-daniel <danielramirez0137@gmail.com>
2017-12-17 16:00:53 -0800

    Finished

--- final_challenge_functions.js
+++ final_challenge_functions.js
@@ -1,5 +1,5 @@
 function checkPrime(number) {
-    for(i = 3; i <= (number)/2; i++); {
+    for(i = 2; i <= (number)/2; i++); {
         if(number % i == 0) {
            return "NOT PRIME";
            }
